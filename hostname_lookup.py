543bdef001c1ba7fc30662cc1f1e740d20c2db3f
Kasper Karlsson <kasper.karlsson@omegapoint.se>
2021-06-23 01:24:04 +0200

    Add page title per protocol

--- hostname_lookup.py
+++ hostname_lookup.py
@@ -1,3 +1,5 @@
+import html
+import re
 import requests
 import urllib3
 
@@ -6,12 +8,14 @@ class Settings:
     FILENAME = "hostnames.txt"
     OUTPUT_FILE = "hosts.html"
     REPORT_TEMPLATE_FILE = "resources/report_template.html"
+    MAX_TITLE_LENGTH = 50
 
 
 class HostResult:
     def __init__(self):
         self.status_codes = {"http": "null", "https": "null"}
         self.resp_length = {"http": "null", "https": "null"}
+        self.title = {"http": "", "https": ""}
 
 
 def main() -> None:
@@ -27,6 +31,7 @@ def lookup_hostnames(hostnames: list):
         try:
             f.write(report_template)
             len_hostnames = len(hostnames)
+            title_regex = re.compile("<title>(.{0,1000})</title>", re.IGNORECASE)
             for i in range(len_hostnames):
                 host = hostnames[i]
                 print(f"{i+1}/{len_hostnames}", host, end="\t")
@@ -37,6 +42,16 @@ def lookup_hostnames(hostnames: list):
                         response = requests.get(url, timeout=0.5, verify=False)
                         result.status_codes[protocol] = str(response.status_code)
                         result.resp_length[protocol] = str(len(response.text))
+                        # Extract title
+                        title = title_regex.search(response.text)
+                        if title:
+                            title_decoded = html.unescape(title.group(1))
+                            title_cut = title_decoded[:Settings.MAX_TITLE_LENGTH]
+                            clean_title = title_cut\
+                                .replace("\\", "\\\\")\
+                                .replace("/", "\\/")\
+                                .replace("'", "\\'")
+                            result.title[protocol] = clean_title
                     except requests.exceptions.Timeout:
                         pass
                     except requests.exceptions.ConnectionError:
@@ -44,13 +59,16 @@ def lookup_hostnames(hostnames: list):
                     except Exception as e:
                         print(f"Uncaught exception: {type(e)} - {e}")
                 print(f"http:[{result.status_codes['http']} {result.resp_length['http']}]\t"
-                      f"https:[{result.status_codes['https']} {result.resp_length['https']}]")
+                      f"https:[{result.status_codes['https']} {result.resp_length['https']}]\t"
+                      f"title:['{result.title['http']}' / '{result.title['https']}']")
                 f.write("<script>results.push({"
                         f"host:'{host}',"
                         f"http_code:{result.status_codes['http']},"
                         f"http_len:{result.resp_length['http']},"
                         f"https_code:{result.status_codes['https']},"
-                        f"https_len:{result.resp_length['https']}"
+                        f"https_len:{result.resp_length['https']},"
+                        f"http_title:'{result.title['http']}',"
+                        f"https_title:'{result.title['https']}'"
                         "});</script>\n")
                 f.flush()
         finally:
